622f600078cff6fb5040e76018eefe90f9acf238
Dimelo Waterson <54084132+boner-cmd@users.noreply.github.com>
2021-06-18 01:55:45 -0500

    reorganized to put functions up front, updated TODOs

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot
 node_url = "https://wallet.burstcoin.ro/burst?requestType="
 mining_info_url = node_url + "getMiningInfo"
 chain_status_url = node_url + "getBlockchainStatus"
-exchange_info_url = "https://myexchangeinfobucketthing.com"  # placeholder for a bucket with the data
+exchange_info_url = "https://myexchangeinfobucketthing.com"  # TODO replace this with an AWS bucket link
 
 TERABYTE = 10 ** 12
 TEBIBYTE = 2 ** 40
@@ -21,57 +21,44 @@ def get_api(url):
     try:
         dict_response = response.json()
         return dict_response
-    except json.JSONDecodeError:  # check to ensure this doesn't need to be simplejson.JSONDecodeError
+    except json.JSONDecodeError:  # TODO ensure this doesn't need to be simplejson.JSONDecodeError
         print("Response body does not contain valid JSON.")
 
 
-def get_prices():  # placeholder for actually getting the price of Signum
+def get_prices():  # TODO actually query the AWS bucket for price data, throw exception if it fails
     return 0.02
 
 
+def plot_with_commit(target_commitment, plot_size):
+    commitment_ratio = target_commitment / average_commitment  # TODO try/catch this for div by zero
+    commitment_factor = commitment_ratio ** 0.4515449935  # this is an approximation
+    return plot_size * commitment_factor  # returns effective plot space for a given commitment
+
+
+def chance_to_win(effective_space, net_space):
+    return effective_space / net_space  # TODO try/catch this for div by zero
+
+
+budget_cents = abs(int(float(input("Enter your maximum budget, in USD: ")) * 100))
+cost_per_tb = abs(int(float(input("Enter your hard drive price per TB, in USD: ")) * 100))
+if cost_per_tb == 0:
+    print('Cost of hard drive space was entered as 0- just buy Signa then!')
+    raise SystemExit
+max_tb = budget_cents / cost_per_tb
+unit_drive = float(input("Enter your hard drive size in TB: "))
+print('Checking exchange data cache server for price data.')
+cost_per_signa = get_prices()
+signa_per_tb = cost_per_tb / cost_per_signa
+print('Checking node for chain data.')
 mining_info = get_api(mining_info_url)
 chain_status = get_api(chain_status_url)
-
-budget_cents = int(float(input("Enter your maximum budget, in USD: ")) * 100)
-cost_per_tb = int(float(input("Enter your hard drive price per TB, in USD: ")) * 100)
-max_tb = budget_cents / cost_per_tb  # TODO if cost per TB is 0, output to spend the whole budget on Signa and terminate
-tb_increment = 1  # increment to use for each terabyte hard drive
-cost_per_signa = get_prices()
-signaPerTB = cost_per_tb / cost_per_signa  # ratio of cost of committed burst to cost of hard drive space
-net_space = chain_status["cumulativeDifficulty"]  # in bytes
+net_space_bytes = chain_status["cumulativeDifficulty"]
 average_commitment = round(float(mining_info["averageCommitmentNQT"]) / 1e8)  # average in whole burst
 
-
-# per jjos no need to use estimated capacity, can just use real capacity
-
-# GENESIS_TARGET = 18325193796
-# nRecentBlocksMined = 1
-# BASE_TARGET = miningInfo["baseTarget"]
-# capacityEstimationBlocks = 360  # 360 if miner has forged 3 or more blocks in the past 360 blocks, 32400 otherwise"
-# estimatedCapacity = GENESIS_TARGET/1.83*nRecentBlocksMined/(BASE_TARGET * capacityEstimationBlocks)
-
-
-def effective_space(target_commitment, plot_size):
-    commitment_ratio = target_commitment / average_commitment
-    commitment_factor = commitment_ratio ** 0.4515449935  # this is an approximation
-    return plot_size * commitment_factor
-
-
-def chance_to_win(user_space):
-    return user_space / net_space  # user_space is another name for effective_space
-
-
-def build_table():
-    min_step = 1 if tb_increment > 1 else tb_increment
-    max_step = max_tb if max_tb % min_step == 0 else max_tb  # TODO change "else max_tb" to a different expression
-    # else expression must evaluate to less than max_tb
-    tb_steps = [step for step in range(min_step, max_step, tb_increment)]
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+# start building the table
+min_step = 1 if unit_drive > 1 else unit_drive
+max_step = max_tb if max_tb % min_step == 0 else max_tb - (max_tb % min_step)
+tb_steps = [step for step in range(min_step, max_step, unit_drive)]
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
